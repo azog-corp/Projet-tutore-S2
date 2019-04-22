77ff6f6208ab0ede6d9b8e26fa4273a0fec9ba30
Maël Le Folgoc Pontis <maellefolgocpontis@yahoo.fr>
2019-04-22 15:58:49 +0200

    Update Partie.java

--- crapouille/src/crapouille/game/Partie.java
+++ crapouille/src/crapouille/game/Partie.java
@@ -10,17 +10,40 @@ import crapouille.Plateau;
  */
 public class Partie {
 	
-	private static Plateau[][] plateau;
+	private static Plateau plateau;
 	private static Pion[] grenouille;
 	private static Pion[] crapaud;
 	private static Action action;
 
+	public static void setCrapaud(int nbCrapaud) {
+		crapaud = new Pion[nbCrapaud];
+		for (int i = 0; i < crapaud.length; i++) {
+			crapaud[i] = new Pion(i, i, true); 
+			// TODO : changer les coordonées
+		}
+	}
+	
+	public static void setGrenouille(int nbGrenouille) {
+		grenouille = new Pion[nbGrenouille];
+		for (int i = 0; i < grenouille.length; i++) {
+			grenouille[i] = new Pion(i, i, false);
+			// TODO : changer les coordonées
+		}
+	}
+	
+	public static void setPlateau(int ligne, int colonne) {
+		plateau = new Plateau(ligne, colonne);
+	}
+
 	public static void main(String[] args) {
 		int abscisse = 5, // Nombre de colonne
 		ordonnee = 5, // Nombre de ligne
-		nbPion = 20, // Nombre de batracien
+		nbCrapaud = 10, // Nombre de crapaud
+		nbGrenouille = 10, // Nombre de grenouille
 		ordinateur = 0; // Difficulte de l'ordinateur (0 signifie une partie contre un joueur)
-
+		setPlateau(abscisse, ordonnee);
+		setGrenouille(nbGrenouille);
+		setCrapaud(nbCrapaud);
 		System.out.println("Tout c'est bien passer");
 	}
 }
