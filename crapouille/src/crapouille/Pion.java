9c2b8102d817fa7a52e8994238c900332a7d9213
Epoxydel <47179646+Epoxydel@users.noreply.github.com>
2019-04-30 19:09:52 +0200

    Creation d'un test
    Creation d'un test pour la fonction setAbscisse

--- crapouille/src/crapouille/Pion.java
+++ crapouille/src/crapouille/Pion.java
@@ -1,111 +1,130 @@
-/*
- * 
- */
 package crapouille;
 
-/**
- * 
- * @author Azog-corp
- *
- */
+import crapouille.Pion;
+
+
 public class Pion {
 
-	// Création des attributs de l'objet Pion
-	// abscisse du pion
-	// ordonnee du Pion
-	// Le Pion est-il un crapaud ou une grenouille
-	// Le pion est il bloqué
+
 	private int abscisse,
 	ordonnee;
 	private boolean crapaud,
 	bloque;
 
-	/**
-	 * Création d'un pion
-	 * @param abscisse
-	 * @param ordonnee
-	 * @param crapaud
-	 */
 	public Pion(int abscisse, int ordonnee, boolean crapaud) {
-		if (abscisse < 0 || ordonnee < 0) {
-			throw new RuntimeException ("Les coordonnées doivent être positives");
-		}
-		this.abscisse = abscisse;
-		this.ordonnee = ordonnee;
+		this.setAbscisse(abscisse);
+		this.setOrdonnee(ordonnee);
 		this.crapaud = crapaud;
 	}
 
+	/**  
+	 * Vérifie si l'ordonnee d'un pion est positif lors de l'initialisation
+	 * @param ordonnee, valeur rentré par l'utilisateur pour l'ordonee du pion
+	 * sur le plateau
+	 */
+	private void setOrdonnee(int ordonnee) {
+		if ( ordonnee < 0) {
+			System.out.println("Vous ne pouvez pas mettre d'abscisse négative");
+		} else {
+			this.abscisse = ordonnee;
+		}
+	}
+
+	/**  
+	 * Vérifie si l'abscisse d'un pion est positif lors de l'initialisation
+	 * @param abscisse, valeur rentré par l'utilisateur pour l'ordonee du pion
+	 * sur le plateau
+	 */
+	private void setAbscisse(int abscisse) {
+		if ( abscisse < 0) {
+			System.out.println("Vous ne pouvez pas mettre d'abscisse négative");
+		} else {
+			this.abscisse = abscisse;
+		}
+	}
+
 	/**
-	 * 
-	 * @return l'abscisse du pion
+	 * Renvoie la valeur de l'asbscisse de l'objet Pion demandé
+	 * @return l'abscisse du pion demandé
 	 */
 	public int getAbscisse() {
 		return abscisse;
 	}
 
 	/**
-	 * 
-	 * @return l'ordonnee du pion
+	 * Renvoie la valeur de l'ordonnee de l'objet Pion demandé
+	 * @return l'ordonnee du pion demandé
 	 */
 	public int getOrdonnee() {
 		return ordonnee;
 	}
 
 	/**
-	 * Change l'absisse d'un pion après déplacement
-	 * @param abscisse
-	 * @param plateau
+	 * Fonction permettant de bouger un pion sur un tableau en vérifiant si le pion n'est pas bloqué.<br/>
+	 * Si le poin est un crapaud la fonction lui ajoute +1 a son abscisse si la 1er case devant lui est 
+	 * libre ou ajoute +2 si la deuxieme case est libre.
+	 * Si le poin est une grenouille la fonction lui ajoute -1 a son abscisse si la 1er case devant lui est 
+	 * libre ou ajoute -2 si la deuxieme case est libre.
+	 * @param abscisse, L'abscisse ou ce situe le pion
+	 * @param plateau, tableau contenant des Pions
 	 */
-	public void setOrdonnee(int abscisse, Pion[][] pion) {
+	public void setAbscisse(int abscisse, Pion[][] plateau) {
+		// Si le pion est un crapaud est que la première case de droite est vide
+		if (crapaud && plateau[this.abscisse+1][this.ordonnee] == null) {
+			this.abscisse = abscisse+1;
+		// Si le pion est un crapaud est que la deuxième case de droite est vide
+		} else if (crapaud && plateau[this.abscisse+2][this.ordonnee] == null) {
+			this.abscisse = abscisse+2;
 		// Si le pion est une grenouille est que la première case de droite est vide
-		if (!crapaud && this.ordonnee < pion[0].length-1 && pion[this.abscisse][this.ordonnee+1] == null) {
-			this.ordonnee = ordonnee+1;
-			// Si le pion est une grenouille est que la deuxième case de droite est vide
-		} else if (!crapaud && this.ordonnee < pion[0].length-2 && pion[this.abscisse][this.ordonnee+2] == null) {
-			this.ordonnee = ordonnee+2;
-			// Si le pion est un crapaud est que la première case de gauche est vide
-		} else if (crapaud && this.ordonnee > 0 && pion[this.abscisse][this.ordonnee-1] == null) {
-			this.ordonnee = ordonnee-1;
-			// Si le pion est un crapaud est que la deuxième case de gauche est vide
-		} else if (crapaud && this.ordonnee > 1 && pion[this.abscisse][this.ordonnee-2] == null) {
-			this.ordonnee = ordonnee-2;
-		} else {
-			System.out.println("aucun changement");
+		} else if (!crapaud && plateau[this.abscisse-1][this.ordonnee] == null) {
+			this.abscisse = abscisse-1;
+		// Si le pion est une grenouille est que la deuxième case de droite est vide
+		} else if (!crapaud && plateau[this.abscisse-2][this.ordonnee] == null) {
+			this.abscisse = abscisse-2;
 		}
 	}
 
 	/**
-	 * Détermine si le pion est bloqué
-	 * @param pion
+	 * Vérifie si un pion est bloqué :
+	 * <ol><li>- Si un pion n'est pas bloqué, il ajoute la valeur false à 
+	 *         la propriéter bloque de l'objet Pion.</li>
+	 *     <li>- Si un pion est bloqué, il ajoute la valeur true à 
+	 *         la propriéter bloque de l'objet Pion.</li></ol>
+	 * @param bloque, boolean indiquant si un pion est bloqué ou non
+	 * @param plateau, tableau contenant des Pions
 	 */
-	public void setBloque(Pion[][] pion) {
-		// Si le pion est une grenouille est que la première case de droite est vide
-		if ((!crapaud && this.ordonnee < pion[0].length-1 && pion[this.abscisse][this.ordonnee+1] == null) ||
-				// Si le pion est une grenouille est que la deuxième case de droite est vide
-				(!crapaud && this.ordonnee < pion[0].length-2 && pion[this.abscisse][this.ordonnee+2] == null) ||
-				// Si le pion est un crapaud est que la première case de gauche est vide
-				(crapaud && this.ordonnee > 0 && pion[this.abscisse][this.ordonnee-1] == null) ||
+	public void setBloque(boolean bloque, Pion[][] plateau) {
+		// Si le pion est un crapaud est que la première case de gauche est vide
+		if ((crapaud && plateau[this.abscisse+1][this.ordonnee] == null) || 
 				// Si le pion est un crapaud est que la deuxième case de gauche est vide
-				(crapaud && this.ordonnee > 1 && pion[this.abscisse][this.ordonnee-2] == null)) {
+				(crapaud && plateau[this.abscisse+2][this.ordonnee] == null) ||
+				// Si le pion est une grenouille est que la première case de droite est vide
+				((!crapaud && plateau[this.abscisse-1][this.ordonnee] == null) || 
+						// Si le pion est une grenouille est que la première case de droite est vide
+						(!crapaud && plateau[this.abscisse-2][this.ordonnee] == null))) {
 			this.bloque = false; // Le pion n'est pas bloqué
 		} else {
 			this.bloque = true; // Le pion est bloqué
 		}
 	}
 
-	/**
-	 * 
-	 * @return le type du pion
-	 */
 	public boolean isCrapaud() {
 		return crapaud;
 	}
 
-	/**
-	 * 
-	 * @return true si le pion est bloqué
-	 */
 	public boolean isBloque() {
 		return bloque;
 	}
+
+	/**
+	 * Renvoie une représentation sous forme de chaîne de l'objet d'un Pion
+	 * @return un string représentant un Pion
+	 */
+	@Override
+	public String toString() {
+		return "Pion(" + abscisse + "," + ordonnee + ") crapaud=" + crapaud + ", bloque=" + bloque
+				+ "]";
+	}
+	
+	
 }
--- crapouille/src/crapouille/test/TestPion.java
+++ crapouille/src/crapouille/test/TestPion.java
@@ -5,8 +5,63 @@ import java.util.Scanner;
 
 public class TestPion {
 	
+	/* Initialisation  de l'analyseur lexical */
     static Scanner entree = new Scanner(System.in);
-	
+    
+    /* initialisation des pions pour les test */
+    static Pion pionTest1 = new Pion(0,1,true);
+
+	static Pion pionTest2 = new Pion(0,2,true);
+
+	static Pion pionTest3 = new Pion(1,2,false);
+
+	static Pion pionTest4 = new Pion(0,3,true);
+
+	static Pion pionTest5 = new Pion(1,3,false);
+
+	static Pion pionTest6 = new Pion(2,3,false);
+
+	static Pion pionTest7 = new Pion(4,4,true);
+
+	static Pion pionTest8 = new Pion(4,5,false);
+
+	static Pion pionTest9 = new Pion(3,6,true);
+
+	static Pion pionTest10 = new Pion(4,6,false);
+
+	static Pion pionTest11 = new Pion(2,7,true);
+
+	static Pion pionTest12 = new Pion(3,7,true);
+
+	static Pion pionTest13 = new Pion(4,7,false);
+
+	static Pion pionTest14 = new Pion(0,8,false);
+         
+    
+    
+    /*
+     * Tableau de jeu contenant des pion pour la vérification de la fonction set 
+     */
+	static Pion[][] jeuTest = {
+			{},
+			{pionTest1},
+			{pionTest2,pionTest3},
+			{pionTest4,pionTest5,pionTest6},
+			{pionTest7},
+			{pionTest8},
+			{pionTest9,pionTest10},
+			{pionTest11,pionTest12,pionTest13},
+			{pionTest14}
+	};
+    
+    
+    /**
+     * Test usuel permettant de vérifier la création d'un pion
+     * Un pion est initailiser avec les valeur suivante :
+     * <ol><li>abscisse = 0</li>
+     *     <li>ordonnee = 0</li>
+     *     <li>crapaud  = false</li></ol>
+     */
 	public static void testCreationPion() {
 		int abscisse = 0,
 		    ordonnee = 0;
@@ -59,7 +114,82 @@ public class TestPion {
 		}
 	}
 
+	public static void testSetAbscisse() {
+		int abscisseDebut,
+		    abscisseFin,
+		    nbEchec = 0;
+		
+		System.out.println("----------- Test pour la fonction setAbscisse ----------\n\n");
+		
+		/* Test d'avancement pour un crapaud sans être bloqué */
+		abscisseDebut = pionTest1.getAbscisse();
+		pionTest1.setAbscisse(0, jeuTest);
+		abscisseFin = pionTest1.getAbscisse();
+		if ((abscisseDebut + 1 ) != abscisseFin ) {
+			nbEchec++;
+		}
+		
+		/* Test si un crapaud avancé s'il y a u pion devant lui */
+		abscisseDebut = pionTest2.getAbscisse();
+		pionTest2.setAbscisse(0, jeuTest);
+		abscisseFin = pionTest2.getAbscisse();
+		if ((abscisseDebut + 2 ) != abscisseFin ) {
+			nbEchec++;
+		}
+		
+		/* Test quand un crapaud est bloqué par deux autre Pion */
+		abscisseDebut = pionTest4.getAbscisse();
+		pionTest4.setAbscisse(0, jeuTest);
+		abscisseFin = pionTest4.getAbscisse();
+		if (abscisseDebut != abscisseFin ) {
+			nbEchec++;
+		}
+		
+		/*test si le pion est arriver à  la fin */
+		abscisseDebut = pionTest7.getAbscisse();
+		pionTest7.setAbscisse(4, jeuTest);
+		abscisseFin = pionTest7.getAbscisse();
+		if (abscisseDebut != abscisseFin ) {
+			nbEchec++;
+		}
+		
+		/* Test d'avancement pour un crapaud sans être bloqué */
+		abscisseDebut = pionTest8.getAbscisse();
+		pionTest8.setAbscisse(0, jeuTest);
+		abscisseFin = pionTest8.getAbscisse();
+		if ((abscisseDebut - 1 ) != abscisseFin ) {
+			nbEchec++;
+		}
+		
+		/* Test si un crapaud avancé s'il y a u pion devant lui */
+		abscisseDebut = pionTest10.getAbscisse();
+		pionTest10.setAbscisse(0, jeuTest);
+		abscisseFin = pionTest10.getAbscisse();
+		if ((abscisseDebut - 2 ) != abscisseFin ) {
+			nbEchec++;
+		}
+		
+		/* Test quand un crapaud est bloqué par deux autre Pion */
+		abscisseDebut = pionTest13.getAbscisse();
+		pionTest13.setAbscisse(0, jeuTest);
+		abscisseFin = pionTest13.getAbscisse();
+		if (abscisseDebut != abscisseFin ) {
+			nbEchec++;
+		}
+		
+		/*test si le pion est arriver à  la fin */
+		abscisseDebut = pionTest14.getAbscisse();
+		pionTest14.setAbscisse(0, jeuTest);
+		abscisseFin = pionTest14.getAbscisse();
+		if (abscisseDebut != abscisseFin ) {
+			nbEchec++;
+		}
+		
+		System.out.println("Il y a " + nbEchec + " echec.\n\n");
+	}
+	
 	public static void main(String[] args) {
+		testSetAbscisse();
 		testCreationPion();
 
 	}
