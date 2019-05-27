60672d5d454aa103181cf2075acc4da57b023cbb
Yamkido <pierremarie.combalbert@iut-rodez.fr>
2019-05-27 19:41:02 +0200

    Outils + separation

---
+++ Projet-jeu/crapouille/Outils.java
@@ -0,0 +1,154 @@
+package crapouille;
+
+import crapouille.configuration.Configuration;
+
+public class Outils {
+	/** initialisation des constantes min/max lignes et colonnes */
+	final static int MIN_LIGNE_PION = 1;
+	final static int MAX_LIGNE_PION = 20;
+	final static int MIN_COLONNE_PION = 1;
+	final static int MAX_COLONNE_PION = 20;
+
+	/** initialisation des constantes du format : XXX;XX */
+	final static int NOM_PION = 0; // position du 1er chiffre de l'entier
+	final static int PREMIER_CHIFFRE_LIGNE = 1; // position du 1er chiffre de l'entier
+	final static int DEUXIEME_CHIFFRE_LIGNE = 2; // position du 2e chiffre de l'entier
+	final static int DISTINCTION_1 = 3; // position du 1er caractère ';' dans le format parcourut
+	final static int PREMIER_CHIFFRE_COLONNE = 4; // position du 1er chiffre de l'entier
+	final static int DEUXIEME_CHIFFRE_COLONNE = 5; // position du 2e chiffre de l'entier
+
+	/**
+	 * Verifie si le char correspond correspond a un crapaud
+	 * Si se n'est pas le cas renvoit false se qui signifie que
+	 * le pion est une grenouille. Cette fonction intervient lors
+	 * de la creation du pion dans le tableau afin de definir son type
+	 * Ainsi que apres toutes les verifications sur l'entree de l'utilisateur
+	 * Se qui signifie que le pion a se moment ne pourra etre que C ou G
+	 * @param choix char rentre par l'utilisateur qui definie le type du pion
+	 */
+	public static boolean recupType(char choix) {
+		return choix == 'C' ? true : false;
+	}
+	
+	public static String afficherConfigDispo(){
+		StringBuilder configs = new StringBuilder();
+		for (int compteur = 0; compteur < Configuration.listConfiguration.size(); compteur++) {
+			configs.append(compteur + " - ");
+			configs.append(Configuration.listConfiguration.get(compteur).getNom());
+			configs.append("\n");
+		}
+		System.out.println(configs.toString());
+		return configs.toString();
+	}
+	
+	
+	/**
+	 * Récupère le coordonnée de la ligne du pion
+	 * @param coordonneePion est le coordonné du poin sur le plateau 
+	 * @return le coordonné de la ligne du pion
+	 */
+	public static int recupereLignePion(String coordonneePion) {
+		StringBuilder lignePion = new StringBuilder(); // lignePion est le coordonnée de la ligne du pion
+		lignePion.append(coordonneePion.charAt(0));
+		lignePion.append(coordonneePion.charAt(1));
+		return Integer.parseInt(lignePion.toString());
+	}
+
+	/**
+	 * Récupère le coordonnée de la colonne du pion
+	 * @param coordonneePion est le coordonné du poin sur le plateau 
+	 * @return la coordonné de la colonne du pion
+	 */
+	public static int recupereColonnePion(String coordonneePion) {
+		StringBuilder colonnePion = new StringBuilder(); // lignePion est le coordonnée de la ligne du pion
+		colonnePion.append(coordonneePion.charAt(0));
+		colonnePion.append(coordonneePion.charAt(1));
+		return Integer.parseInt(colonnePion.toString());
+	}
+	
+	/*--------------- FONCTION ESTVALIDE  ---------------*/
+
+	//TODO a tester jamais reessayer de creer config donc a regarder validité
+	public static boolean typeValide(String type) {
+		if (type.length() != 1 || (type.charAt(0) != 'C' && type.charAt(0) != 'G')) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	/**
+	 * Fonction qui vérifie si uen chaine de caractère ne contient que des chiffres
+	 * @param aVerifier String a verifier la validité ( pas de nombre)
+	 * @return false si la string aVerifier contient une lettre
+	 * 		   true si la String ne contient aucune lettre
+	 */
+	public static boolean verificationLettre(String aVerifier) {
+		for (int compteur = 0; compteur < aVerifier.length(); compteur ++) {
+			char tester = aVerifier.charAt(compteur);
+			if (tester < '0' || tester > '9') {
+				return false;
+			}
+		}
+		return true;	
+	}
+	
+	/**
+	 * Détermine si le coordonée de la ligne du pion est correct  
+	 * avec le contrôle de la gestion d'erreur
+	 * @param coordonneePion est le coordonné du poin sur le plateau 
+	 * @return un booleen égal a vrai si la ligne du pion est correct
+	 */
+	public static boolean ligneEstValide(int lignePion) {
+		// La ligne du pion doit être supérieur à 0 et inférieure ou égal à 20  
+		return MIN_LIGNE_PION <= lignePion && lignePion < MAX_LIGNE_PION;
+	}
+
+	/**
+	 * Détermine si la coordonée de la colonne du pion est correct  
+	 * avec le contrôle de la gestion d'erreur
+	 * @param coordonneePion est le coordonné du poin sur le plateau 
+	 * @return un booleen égal a vrai si la colonne du pion est correct
+	 */
+	public static boolean colonneEstValide(int colonnePion) {
+		// La ligne du pion doit être supérieur à 0 et inférieure ou égal à 20  
+		return MIN_COLONNE_PION <= colonnePion && colonnePion < MAX_COLONNE_PION;
+	}	
+	
+	
+	//TODO tester
+	//Normalement pas besoin tester - car caractere non valide
+	public static boolean cordOk(int ligne, int colonne) {
+		return ligne < Partie.config.length && 
+				colonne < Partie.config[0].length &&
+				ligne >= 0 && colonne >= 0;
+	}
+	
+	/**
+	 * Détermine si les lignes du plateau sont corrects 
+	 * avec le contrôle de la gestion d'erreur
+	 * @param colonnePlateau est le nombre de colonnes pour le plateau 
+	 * @param lignePlateau est le nombre de lignes pour le plateau
+	 * @return un booleen égal a vrai si les lignes et colonnes plateau sont corrects
+	 */
+	public static boolean configPlateauEstValide(int lignePlateau, int colonnePlateau) {
+		// vérification des entier ligne et colonne avec 
+		// la taille max et min d'une ligne et d'une colonne
+		// limité a 20 (pour l'instant)
+		return (MIN_LIGNE_PION <= lignePlateau || lignePlateau <= MAX_LIGNE_PION) &&
+				(MIN_COLONNE_PION <= colonnePlateau || colonnePlateau <=MAX_COLONNE_PION);
+	}
+	
+	//TODO vérifier lequipe et afficher la quelle a gagner lb_nomEquipeGagnante
+	public static boolean verifVictoire() {
+		if (Partie.currentPlateau.victoire(0) || 
+				Partie.currentPlateau.victoire(1) || 
+				Partie.currentPlateau.victoire(1)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+	
+	
--- Projet-jeu/crapouille/interfaceFx/InterfaceAppliController.java
+++ Projet-jeu/crapouille/interfaceFx/InterfaceAppliController.java
@@ -9,6 +9,7 @@ package crapouille.interfaceFx;
 
 import java.time.LocalDate;
 
+import crapouille.Outils;
 import crapouille.Partie;
 import crapouille.Pion;
 import crapouille.Plateau;
@@ -29,31 +30,13 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 public class InterfaceAppliController {
-
-
-	/** initialisation des constantes min/max lignes et colonnes */
-	final static int MIN_LIGNE_PION = 1;
-	final static int MAX_LIGNE_PION = 20;
-	final static int MIN_COLONNE_PION = 1;
-	final static int MAX_COLONNE_PION = 20;
-
-	/** initialisation des constantes du format : XXX;XX */
-	final static int NOM_PION = 0; // position du 1er chiffre de l'entier
-	final static int PREMIER_CHIFFRE_LIGNE = 1; // position du 1er chiffre de l'entier
-	final static int DEUXIEME_CHIFFRE_LIGNE = 2; // position du 2e chiffre de l'entier
-	final static int DISTINCTION_1 = 3; // position du 1er caractère ';' dans le format parcourut
-	final static int PREMIER_CHIFFRE_COLONNE = 4; // position du 1er chiffre de l'entier
-	final static int DEUXIEME_CHIFFRE_COLONNE = 5; // position du 2e chiffre de l'entier
-
 	final static String MSGBOX_TITRE = "Crapauds & Grenouilles";
 	final static String MSGBOX_TYPE = "Type non valide";
 	final static String MSGBOX_LETTRE = "Les lettres ne sont pas acceptées";
 	final static String MSGBOX_NOMBRE = "Nombre trop grand ou trop petit vérifier votre saisie";
 	final static String MSGBOX_VIDE = "Ne doit pas être vide";
 	final static String MSBOX_CONFIG = "La configuration ne peut pas être vide";
-	
-	
-	private String nom;
+
 	private String nomConfig;
 
 	final static String MESSAGE_ERREUR = "Les informations rentrés sont invalides : ";
@@ -333,7 +316,7 @@ public class InterfaceAppliController {
 		reinitialiser();
 		razPartie();
 		configurationPartie.setVisible(true);
-		listeConfigDispo.setText(afficherConfigDispo());
+		listeConfigDispo.setText(Outils.afficherConfigDispo());
 	}
 
 	/**
@@ -543,47 +526,40 @@ public class InterfaceAppliController {
 			/* Vérifie s'il n'y a pas de lettre avant de convertir en int les entrées
 			 * pour ne pas produire d'erreur
 			 */
-			if (verificationLettre(ligne) && verificationLettre(colonne)) {
+			if (Outils.verificationLettre(ligne) 
+					&& Outils.verificationLettre(colonne)) {
 				erreurEntreePartie.setVisible(false);
 				/*Conversion en int des entrées texte de l'utilisateur */
 				int colonnePion = Integer.parseInt(colonne);
 				int lignePion = Integer.parseInt(ligne);
-				if (colonneEstValide(colonnePion) && ligneEstValide(lignePion)) {
+				if (Outils.colonneEstValide(colonnePion) 
+						&& Outils.ligneEstValide(lignePion)) {
 					colonnePion--;
 					lignePion--;
 					Partie.tourEntite(lignePion, colonnePion); // TODO verif pion correcte deja faite ?
-					System.out.println(Partie.getCurrentPlateau().toString());
 					rafraichirJeu(Partie.getCurrentPlateau().toString());
 					Partie.setNbCoups(Partie.getNbCoups()+1); //TODO verif casse tt
 				} else {
 					showMsgbox(MSGBOX_TITRE, MESSAGE_ERREUR
-							+ "Nombre trop grand ou trop petit vérifier votre saisie",
-							false);
+							+ MSGBOX_NOMBRE,false);
 				}
 			} else {
 				showMsgbox(MSGBOX_TITRE,MESSAGE_ERREUR
-						+"Ne doit contenir que des chiffres",
+						+MSGBOX_LETTRE,
 						false);
 			}
 		} else {
 			showMsgbox(MSGBOX_TITRE,MESSAGE_ERREUR
-					+"Ne doit pas être vide",
+					+ MSGBOX_VIDE,
 					false);
 		}
 		//TODO enlever défaite
-		verifVictoire();
-	}
-
-	//TODO vérifier lequipe et afficher la quelle a gagner lb_nomEquipeGagnante
-	private void verifVictoire() {
-		if (Partie.currentPlateau.victoire(0) || 
-				Partie.currentPlateau.victoire(1) || 
-				Partie.currentPlateau.victoire(1)) {
+		if (Outils.verifVictoire()) {
 			jeuEnCours.setVisible(false);
 			victoire.setVisible(true);
 		}
 	}
-
+	
 	private void recupModeJeu() {
 		if (chk_casseT.isSelected()) {
 			Partie.setChoixModeDeJeu(0);
@@ -598,12 +574,13 @@ public class InterfaceAppliController {
 		int nConfig = Integer.parseInt(choixConfig.getText());
 		Partie.loadConfig(nConfig);
 	}
+	
 	//TODO
 	private boolean configValide() {
 		/* On vérifie que la la string n'est pas vide pour ne pas produire d'erreur par la suite */
 		if (!choixConfig.getText().isEmpty()) {
 			String configString = choixConfig.getText();
-			if(verificationLettre(configString)) {
+			if(Outils.verificationLettre(configString)) {
 				int config = Integer.parseInt(choixConfig.getText());
 				if (config >= 0 && config < Configuration.listConfiguration.size()) {
 					erreurConfigPartie.setVisible(false);
@@ -671,20 +648,6 @@ public class InterfaceAppliController {
 			Partie.setChoixAdversaire(0);
 		}
 	}
-
-
-	public String afficherConfigDispo(){
-		StringBuilder configs = new StringBuilder();
-		for (int compteur = 0; compteur < Configuration.listConfiguration.size(); compteur++) {
-			configs.append(compteur + " - ");
-			configs.append(Configuration.listConfiguration.get(compteur).getNom());
-			configs.append("\n");
-		}
-		System.out.println(configs.toString());
-		return configs.toString();
-	}
-
-
 	public void rafraichirJeu(String plateauJeu) {
 		gameBoardString.setText(plateauJeu);
 	}
@@ -707,11 +670,11 @@ public class InterfaceAppliController {
 	void configInitialisation(MouseEvent Click) {
 		if (!tb_nbLigneConf.getText().isEmpty() 
 				&& !tb_nbColonneConf.getText().isEmpty()) {
-			if (verificationLettre(tb_nbColonneConf.getText()) 
-					&& verificationLettre(tb_nbColonneConf.getText())) {
+			if (Outils.verificationLettre(tb_nbColonneConf.getText()) 
+					&& Outils.verificationLettre(tb_nbColonneConf.getText())) {
 				int nbLigne = Integer.parseInt(tb_nbLigneConf.getText());
 				int nbColonne = Integer.parseInt( tb_nbColonneConf.getText());
-				if (ligneEstValide(nbLigne) && colonneEstValide(nbColonne)) {
+				if (Outils.ligneEstValide(nbLigne) && Outils.colonneEstValide(nbColonne)) {
 					erreurCreationConfig.setVisible(false);
 					Partie.config = new Pion[nbLigne][nbColonne];
 					recupNomConf(); //recupère le nom pour enregistrer
@@ -744,21 +707,23 @@ public class InterfaceAppliController {
 	@FXML
 	//TODO corriger
 	void ajoutPionConfig(MouseEvent Click) {
+		System.out.println(tb_cordColonne.getText());
 		if (!tb_cordColonne.getText().isEmpty() 
 				&& !tb_cordLigne.getText().isEmpty()) {
 			String colonne = tb_cordColonne.getText();
 			String ligne = tb_cordLigne.getText();
-			if (verificationLettre(ligne) && verificationLettre(colonne)) {
+			if (Outils.verificationLettre(ligne) 
+					&& Outils.verificationLettre(colonne)) {
 				//TODO vérifier que marche sans la fonction (jai supprime la fonction recupereLignePion
 				int colonnePion = Integer.parseInt(colonne);
 				int lignePion = Integer.parseInt(ligne);
 				colonnePion--;
 				lignePion--;
-				if (cordOk(lignePion,colonnePion)) {
-					if (typeValide(tb_cordType.getText())) {
+				if (Outils.cordOk(lignePion,colonnePion)) {
+					if (Outils.typeValide(tb_cordType.getText())) {
 						erreurPlacementPion.setVisible(false);
 						Pion placementUti = new Pion(lignePion,colonnePion,
-								recupType(tb_cordType.getText().charAt(0)));
+							Outils.recupType(tb_cordType.getText().charAt(0)));
 						Partie.config[lignePion][colonnePion] = placementUti;
 						rafraichirConf(Partie.configToString());
 					} else {
@@ -767,18 +732,15 @@ public class InterfaceAppliController {
 					}
 				} else {
 					showMsgbox(MSGBOX_TITRE,
-							MESSAGE_ERREUR + MSGBOX_NOMBRE,
-							false);
+							MESSAGE_ERREUR + MSGBOX_NOMBRE, false);
 				}
 			} else {
 				showMsgbox(MSGBOX_TITRE,
-						MESSAGE_ERREUR + MSGBOX_LETTRE,
-						false);
+						MESSAGE_ERREUR + MSGBOX_LETTRE, false);
 			}
 		} else {
 			showMsgbox(MSGBOX_TITRE,
-					MESSAGE_ERREUR + MSGBOX_VIDE,
-					false);
+					MESSAGE_ERREUR + MSGBOX_VIDE,false);
 		}
 	}
 
@@ -798,7 +760,7 @@ public class InterfaceAppliController {
 			/* Vérifie que tous les caractères de l'information rentrées par l'utilisateur
 			 * sont bien des chiffres
 			 */
-			if (verificationLettre(idConfig)) {
+			if (Outils.verificationLettre(idConfig)) {
 				/* Converti en int le String correspondant au numéro de la config devant être supprimé */
 				int index = Integer.parseInt(tb_idConf.getText());
 				/*Vérifie que le numéro rentré par l'utilisateur correspond bien 
@@ -856,19 +818,6 @@ public class InterfaceAppliController {
 	}
 
 	/**
-	 * Verifie si le char correspond correspond a un crapaud
-	 * Si se n'est pas le cas renvoit false se qui signifie que
-	 * le pion est une grenouille. Cette fonction intervient lors
-	 * de la creation du pion dans le tableau afin de definir son type
-	 * Ainsi que apres toutes les verifications sur l'entree de l'utilisateur
-	 * Se qui signifie que le pion a se moment ne pourra etre que C ou G
-	 * @param choix char rentre par l'utilisateur qui definie le type du pion
-	 */
-	private boolean recupType(char choix) {
-		return choix == 'C' ? true : false;
-	}
-
-	/**
 	 * Recupere le nom que l'utilisateur a entre dans la textBox de creation de 
 	 * configuration si il est vide prend la valeur defaut
 	 * (remplacera donc la configuration par defaut)
@@ -879,7 +828,6 @@ public class InterfaceAppliController {
 	//	}
 	public void recupNomConf() {
 		if (tb_nomConf.getText().isEmpty()) {
-			//TODO pas tester
 			nomConfig = "Configuration n°" + Configuration.listConfiguration.size();
 		} else {
 			nomConfig = tb_nomConf.getText();
@@ -916,7 +864,7 @@ public class InterfaceAppliController {
 	@FXML
 	void showSupprimerConfig(MouseEvent Click) {
 		/*Affiche toutes les configurations disponibles*/
-		configAdel.setText(afficherConfigDispo());
+		configAdel.setText(Outils.afficherConfigDispo());
 		/* Cache la page de sélection ajouter / supprimer config et affiche supprimer config */
 		choixConf.setVisible(false);
 		supprimerConf.setVisible(true);
@@ -956,97 +904,5 @@ public class InterfaceAppliController {
 		msgUti.showAndWait();
 	}
 
-	/*--------------- FONCTION ESTVALIDE  ---------------*/
-	/**
-	 * Fonction qui vérifie si uen chaine de caractère ne contient que des chiffres
-	 * @param aVerifier String a verifier la validité ( pas de nombre)
-	 * @return false si la string aVerifier contient une lettre
-	 * 		   true si la String ne contient aucune lettre
-	 */
-	public boolean verificationLettre(String aVerifier) {
-		for (int compteur = 0; compteur < aVerifier.length(); compteur ++) {
-			char tester = aVerifier.charAt(compteur);
-			if (tester < '0' || tester > '9') {
-				return false;
-			}
-		}
-		return true;	
-	}
-
-	/**
-	 * Détermine si les lignes du plateau sont corrects 
-	 * avec le contrôle de la gestion d'erreur
-	 * @param colonnePlateau est le nombre de colonnes pour le plateau 
-	 * @param lignePlateau est le nombre de lignes pour le plateau
-	 * @return un booleen égal a vrai si les lignes et colonnes plateau sont corrects
-	 */
-	public static boolean configPlateauEstValide(int lignePlateau, int colonnePlateau) {
-		// vérification des entier ligne et colonne avec 
-		// la taille max et min d'une ligne et d'une colonne
-		// limité a 20 (pour l'instant)
-		return (MIN_LIGNE_PION <= lignePlateau || lignePlateau <= MAX_LIGNE_PION) &&
-				(MIN_COLONNE_PION <= colonnePlateau || colonnePlateau <=MAX_COLONNE_PION);
-	}
-
-	/**
-	 * Détermine si le coordonée de la ligne du pion est correct  
-	 * avec le contrôle de la gestion d'erreur
-	 * @param coordonneePion est le coordonné du poin sur le plateau 
-	 * @return un booleen égal a vrai si la ligne du pion est correct
-	 */
-	public static boolean ligneEstValide(int lignePion) {
-		// La ligne du pion doit être supérieur à 0 et inférieure ou égal à 20  
-		return MIN_LIGNE_PION <= lignePion && lignePion < MAX_LIGNE_PION;
-	}
-
-	/**
-	 * Détermine si la coordonée de la colonne du pion est correct  
-	 * avec le contrôle de la gestion d'erreur
-	 * @param coordonneePion est le coordonné du poin sur le plateau 
-	 * @return un booleen égal a vrai si la colonne du pion est correct
-	 */
-	public static boolean colonneEstValide(int colonnePion) {
-		// La ligne du pion doit être supérieur à 0 et inférieure ou égal à 20  
-		return MIN_COLONNE_PION <= colonnePion && colonnePion < MAX_COLONNE_PION;
-	}
-
-	/**
-	 * Récupère le coordonnée de la ligne du pion
-	 * @param coordonneePion est le coordonné du poin sur le plateau 
-	 * @return le coordonné de la ligne du pion
-	 */
-	public static int recupereLignePion(String coordonneePion) {
-		StringBuilder lignePion = new StringBuilder(); // lignePion est le coordonnée de la ligne du pion
-		lignePion.append(coordonneePion.charAt(0));
-		lignePion.append(coordonneePion.charAt(1));
-		return Integer.parseInt(lignePion.toString());
-	}
-
-	/**
-	 * Récupère le coordonnée de la colonne du pion
-	 * @param coordonneePion est le coordonné du poin sur le plateau 
-	 * @return la coordonné de la colonne du pion
-	 */
-	public static int recupereColonnePion(String coordonneePion) {
-		StringBuilder colonnePion = new StringBuilder(); // lignePion est le coordonnée de la ligne du pion
-		colonnePion.append(coordonneePion.charAt(0));
-		colonnePion.append(coordonneePion.charAt(1));
-		return Integer.parseInt(colonnePion.toString());
-	}
-	//TODO a tester jamais reessayer de creer config donc a regarder validité
-	private boolean typeValide(String type) {
-		if (type.length() != 1 || (type.charAt(0) != 'C' && type.charAt(0) != 'G')) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	//TODO tester
-	//Normalement pas besoin tester - car caractere non valide
-	private boolean cordOk(int ligne, int colonne) {
-		return ligne < Partie.config.length && 
-				colonne < Partie.config[0].length &&
-				ligne >= 0 && colonne >= 0;
-	}
 
 }
