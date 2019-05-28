4365aa5b8476192fcf3dd5e925de72d1b016f18f
Epoxydel <47179646+Epoxydel@users.noreply.github.com>
2019-05-28 21:00:18 +0200

    Update TestOutils.java

--- Projet-jeu/crapouille/test/TestOutils.java
+++ Projet-jeu/crapouille/test/TestOutils.java
@@ -4,8 +4,9 @@ import crapouille.Outils;
 
 public class TestOutils {
 
-	
-	/** Fonction test de recupType
+
+	/** 
+	 * Fonction test de recupType
 	 * Test uniquement la bonne recuperation
 	 * Car les differents des cas d'erreurs sont toujours geres
 	 * Avant l'appel de cette fonction
@@ -14,7 +15,7 @@ public class TestOutils {
 		final char[] aTesterChar = {'C', 'G'};
 		final boolean[] recupTypeAttendu = {true, false};
 		int nbTestOk = 0;
-		
+
 		for(int i = 0; i < aTesterChar.length; i++) {
 			if (Outils.recupType(aTesterChar[i]) == recupTypeAttendu[i]) {
 				nbTestOk++;
@@ -22,10 +23,6 @@ public class TestOutils {
 		}
 		System.out.println(nbTestOk + " sur " + aTesterChar.length + " tests");
 	}
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		//TODO c'est quoi  ce bordel inchallah
-=======
 
 	/**
 	 * Fonction test de EstValide
@@ -70,32 +67,73 @@ public class TestOutils {
 	}
 
 	/**
-	 * Fonction test de verificationLettre
+	 * Fonction test de ligneEstValide
 	 * Test uniquement la bonne recuperation
 	 * Car les differents des cas d'erreurs sont toujours geres
 	 * Avant l'appel de cette fonction
 	 */
 	private static void testLigneEstValide () {
 		final int[] aTesterLigne = {0,-1,5,10,31,40,3,20};
-		final boolean[] recupAttendu = {true,false,true,true,false,false,true,true};
+		final boolean[] recupAttendu = {false,false,true,true,false,false,true,true};
 		int nbTestOk = 0;
 
 		for(int i = 0; i < aTesterLigne.length; i++) {
 			/* Vérifie si la ligne est valide et compare avec le tableau résultat */
 			if (Outils.ligneEstValide(aTesterLigne[i]) == recupAttendu[i] ) {
 				nbTestOk++;
-				System.out.println(i);
 			}
 		}
 
 		System.out.println("il y a " + nbTestOk + " test réussie sur 8");
 	}
 
+	/**
+	 * Fonction test de colonneEstValide
+	 * Test uniquement la bonne recuperation
+	 * Car les differents des cas d'erreurs sont toujours geres
+	 * Avant l'appel de cette fonction
+	 */
+	private static void testColonneEstValide () {
+		final int[] aTesterColonne = {0,-1,5,10,31,40,3,20};
+		final boolean[] recupAttendu = {false,false,true,true,false,false,true,true};
+		int nbTestOk = 0;
 
+		for(int i = 0; i < aTesterColonne.length; i++) {
+			/* Vérifie si la ligne est valide et compare avec le tableau résultat */
+			if (Outils.colonneEstValide(aTesterColonne[i]) == recupAttendu[i] ) {
+				nbTestOk++;
+			}
+		}
+
+		System.out.println("il y a " + nbTestOk + " test réussie sur 8");
+	}
+	
+	/**
+	 * Fonction test de configPlateauEstValide
+	 * Test uniquement la bonne recuperation
+	 * Car les differents des cas d'erreurs sont toujours geres
+	 * Avant l'appel de cette fonction
+	 */
+	private void TestconfigPlateauEstValide () {
+		final int[] aTesterLigne = {0,20,2,6,31,-1,2};
+		final int[] aTesterColonne = {0,20,2,9,31,2,-1};
+		final boolean[] recupAttendu = {true,true,true,true,false,false,false};
+		int nbTestOk = 0;
+		
+		for(int i = 0; i < aTesterColonne.length; i++) {
+			/* Vérifie si la ligne et les colone sont valide et compare avec le tableau résultat */
+			if (Outils.configPlateauEstValide(aTesterLigne[i],aTesterColonne[i]) == recupAttendu[i] ) {
+				nbTestOk++;
+			}
+		}
+
+		System.out.println("il y a " + nbTestOk + " test réussie sur 8");
+		
+	}
 	public static void main(String[] args) {
 		testEstValide();
 		testVerificationLettre ();
-		testLigneEstValide ();
->>>>>>> parent of 0be4e4a... Merge branch 'master' of https://github.com/azog-corp/Projet-tutore-S2
+		 testLigneEstValide ();
+		 testColonneEstValide ();
 	}
 }
