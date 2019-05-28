319e4cddd6d4400ccfe7bc8adf6aa0fe46efcace
Yamkido <pierremarie.combalbert@iut-rodez.fr>
2019-05-28 19:50:36 +0200

    testOutils

--- Projet-jeu/crapouille/test/TestOutils.java
+++ Projet-jeu/crapouille/test/TestOutils.java
@@ -4,8 +4,9 @@ import crapouille.Outils;
 
 public class TestOutils {
 
-	
-	/** Fonction test de recupType
+
+	/** 
+	 * Fonction test de recupType
 	 * Test uniquement la bonne recuperation
 	 * Car les differents des cas d'erreurs sont toujours geres
 	 * Avant l'appel de cette fonction
@@ -14,7 +15,7 @@ public class TestOutils {
 		final char[] aTesterChar = {'C', 'G'};
 		final boolean[] recupTypeAttendu = {true, false};
 		int nbTestOk = 0;
-		
+
 		for(int i = 0; i < aTesterChar.length; i++) {
 			if (Outils.recupType(aTesterChar[i]) == recupTypeAttendu[i]) {
 				nbTestOk++;
@@ -22,7 +23,75 @@ public class TestOutils {
 		}
 		System.out.println(nbTestOk + " sur " + aTesterChar.length + " tests");
 	}
+
+	/**
+	 * Fonction test de EstValide
+	 * Test uniquement la bonne recuperation
+	 * Car les differents des cas d'erreurs sont toujours geres
+	 * Avant l'appel de cette fonction
+	 */
+	private static void testEstValide () {
+		final String[] aTesterString = {"C","G","A","R","Z","AA","1"};
+		final boolean[] recupAttendu = {true,true,false,false,false,false,false};
+		int nbTestOk = 0;
+
+		for(int i = 0; i < aTesterString.length; i++) {
+			/* Vérifie si le String est valide et compare avec le tableau résultat */
+			if (Outils.typeValide(aTesterString[i]) == recupAttendu[i] ) {
+				nbTestOk++;
+			}
+		}
+
+		System.out.println("il y a " + nbTestOk + " test réussie sur 7");
+	}
+
+	/**
+	 * Fonction test de verificationLettre
+	 * Test uniquement la bonne recuperation
+	 * Car les differents des cas d'erreurs sont toujours geres
+	 * Avant l'appel de cette fonction
+	 */
+	private static void testVerificationLettre () {
+		final String[] aTesterString = {"1234","e234","EArT6","ERTEZ"};
+		final boolean[] recupAttendu = {true,false,false,false};
+		int nbTestOk = 0;
+
+		for(int i = 0; i < aTesterString.length; i++) {
+			/* Vérifie si le String est valide et compare avec le tableau résultat */
+			if (Outils.verificationLettre(aTesterString[i]) == recupAttendu[i] ) {
+				nbTestOk++;
+			}
+		}
+
+		System.out.println("il y a " + nbTestOk + " test réussie sur 4");
+	}
+
+	/**
+	 * Fonction test de verificationLettre
+	 * Test uniquement la bonne recuperation
+	 * Car les differents des cas d'erreurs sont toujours geres
+	 * Avant l'appel de cette fonction
+	 */
+	private static void testLigneEstValide () {
+		final int[] aTesterLigne = {0,-1,5,10,31,40,3,20};
+		final boolean[] recupAttendu = {true,false,true,true,false,false,true,true};
+		int nbTestOk = 0;
+
+		for(int i = 0; i < aTesterLigne.length; i++) {
+			/* Vérifie si la ligne est valide et compare avec le tableau résultat */
+			if (Outils.ligneEstValide(aTesterLigne[i]) == recupAttendu[i] ) {
+				nbTestOk++;
+				System.out.println(i);
+			}
+		}
+
+		System.out.println("il y a " + nbTestOk + " test réussie sur 8");
+	}
+
+
 	public static void main(String[] args) {
-		//TODO c'est quoi  ce bordel inchallah
+		testEstValide();
+		testVerificationLettre ();
+		testLigneEstValide ();
 	}
 }
